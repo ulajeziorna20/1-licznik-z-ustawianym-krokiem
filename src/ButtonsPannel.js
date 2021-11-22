02df85c7d0f79f37fb7e73f392a872e03f68f586
Ula Jeziorna <jeziornaula@wp.pl>
2021-11-22 12:43:36 +0100

    Komponenty ButtonsPanel i Display działają--> znlezienie błędu w budowie funkcji strzałkowej

--- src/ButtonsPannel.js
+++ src/ButtonsPannel.js
@@ -9,9 +9,9 @@ function ButtonsPannel(props) {
 
     return (
         <div>
-            <button className="add-1" onClick={() = props.buttonMethod('add-1')}>Add 1</>button>
-            <button className="reset" onClick={() = props.buttonMethod('reset')}>Reset</button>
-            <button className="reset" onClick={() = props.buttonMethod('reinit')}>Reinit</button>
+            <button className="add-1" onClick={() => props.buttonMethod('add-1')}>Add 1</button>
+            <button className="reset" onClick={() => props.buttonMethod('reset')}>Reset</button>
+            <button className="reset" onClick={() => props.buttonMethod('reinit')}>Reinit</button>
         </div>
     )
 }
