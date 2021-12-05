c5ff606e11a29475da09dffa1a0edaaafe6dd1d8
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-05 15:31:07 +0100

    Formatowanie kodu.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,81 +1,63 @@
-import React from "react";
-import { Component } from "react";
-import ButtonsPannel from "./ButtonsPannel";
-import './css/Counter.css';
-import Display from "./Display";
-import Step from "./Step";
-
-
+import React from "react"
+import { Component } from "react"
+import ButtonsPannel from "./ButtonsPannel"
+import "./css/Counter.css"
+import Display from "./Display"
+import Step from "./Step"
 
 class Counter extends Component {
+  constructor(props) {
+    super(props)
 
-
-    constructor(props) {
-        super(props)
-
-
-        this.state = ({
-            counterValue: this.props.initValue,
-            stepValue: 1
-        })
+    this.state = {
+      counterValue: this.props.initValue,
+      stepValue: 1,
     }
+  }
 
+  stepValueChange = (e) => {
+    // console.log(e.target.value);
+    // console.log(+e.target.value);
 
+    this.setState({
+      stepValue: +e.target.value,
+    })
+  }
 
-    stepValueChange = (e) => {
-        // console.log(e.target.value);
-        // console.log(+e.target.value);
+  buttonHandleChange = (action) => {
+    this.setState((prevState, prevProps) => {
+      let currentCounterValue = prevState.counterValue
+      let currentStepValue = prevState.stepValue
+      console.log(currentStepValue)
+      let arr = currentCounterValue + currentStepValue
+      console.log(arr)
 
-        this.setState({
-            stepValue: +e.target.value
-        })
-    }
+      if (action === "add-x") {
+        currentCounterValue += currentStepValue
+      } else if (action === "reset") {
+        currentCounterValue = 0
+      } else {
+        currentCounterValue = prevProps.initValue
+      }
 
+      return {
+        counterValue: currentCounterValue,
+      }
+    })
+  }
 
-
-
-    buttonHandleChange = (action) => {
-
-        this.setState((prevState, prevProps) => {
-
-
-            let currentCounterValue = prevState.counterValue
-            let currentStepValue = prevState.stepValue
-            console.log(currentStepValue);
-            let arr = currentCounterValue + currentStepValue
-            console.log(arr);
-
-            if (action === 'add-x') {
-                currentCounterValue += currentStepValue
-
-            } else if (action === 'reset') {
-                currentCounterValue = 0
-            } else {
-                currentCounterValue = prevProps.initValue
-
-            }
-
-
-            return ({
-                counterValue: currentCounterValue
-            })
-        })
-    }
-
-
-
-    render() {
-
-        return (
-            <div>
-                <Display counterValueToDisplay={this.state.counterValue} />
-                <ButtonsPannel buttonMethod={this.buttonHandleChange}
-                    stepToDisplay={this.state.stepValue} />
-                <Step stepMethod={this.stepValueChange} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Display counterValueToDisplay={this.state.counterValue} />
+        <ButtonsPannel
+          buttonMethod={this.buttonHandleChange}
+          stepToDisplay={this.state.stepValue}
+        />
+        <Step stepMethod={this.stepValueChange} />
+      </div>
+    )
+  }
 }
 
-
-export default Counter;
+export default Counter
